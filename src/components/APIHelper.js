ce632c5871f01576607f154b774ac2200e2dfc10
SebuBergman <basti.bergman@gmail.com>
2023-03-13 15:30:27 +0200

    added async functions to APIHelper for creating, updating, deleting and finding todos

--- src/components/APIHelper.js
+++ src/components/APIHelper.js
@@ -2,3 +2,26 @@ import axios from "axios";
 
 const API_URL = "http://127.0.0.1:5000/todos/"
 
+async function createTodo(task) {
+    const { data: newTodo } = await axios.post(API_URL, {
+        task,
+    })
+    return newTodo
+}
+
+async function updateTodo(id, payload) {
+    const { data: newTodo } = await axios.put(`${API_URL}${id}`, payload)
+    return newTodo
+}
+
+async function deleteTodo(id) {
+    const message = await axios.delete(`${API_URL}${id}`)
+    return message
+}
+
+async function getAllTodos() {
+    const { data: todos } = await axios.get(API_URL)
+    return todos
+}
+
+export default { createTodo, updateTodo, deleteTodo, getAllTodos }
